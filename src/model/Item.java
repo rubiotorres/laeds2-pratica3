6fd95ffc78eb03e5011f257d30a53755ba164884
rubiotorres <rubiotorres15@gmai.com>
2018-04-13 15:29:51 -0300

    Codigo do Ziviani

---
+++ src/model/Item.java
@@ -0,0 +1,28 @@
+package model;
+
+/**
+ *
+ * @author r-torres
+ */
+public class Item {
+
+    private int chave;
+
+    public Item(int chave) {
+        this.chave = chave;
+    }
+
+    public int compara(Item it) {
+        Item item = it;
+        if (this.chave < item.chave) {
+            return -1;
+        } else if (this.chave > item.chave) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+}
